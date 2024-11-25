37303a2432fedbe98828d5c46aa146141c52f2a9
Phanupong <pphanupong.tle@gmail.com>
2024-11-26 01:41:54 +0700

    CLEAN: Clean authModule

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
-import { UsersModule } from '../users/users.module';
 import { Users } from '../users/entities/users.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Users]), UsersModule],
+  imports: [TypeOrmModule.forFeature([Users])],
   providers: [AuthService],
   controllers: [AuthController],
 })
